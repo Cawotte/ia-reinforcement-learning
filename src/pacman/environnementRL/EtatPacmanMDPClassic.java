6048125bf06d6b703925f31c23745a49e9816313
Cawotte <khamaz.ee@gmail.com>
2019-04-10 23:41:07 +0200

    Externalized Feature Functions in a new class outside of EtatPacman

--- src/pacman/environnementRL/EtatPacmanMDPClassic.java
+++ src/pacman/environnementRL/EtatPacmanMDPClassic.java
@@ -33,65 +33,34 @@ public class EtatPacmanMDPClassic implements Etat , Cloneable{
 
 		distanceGhost = 0;
 		directionGhost = MazePacman.NORTH; //arbitrary default value
+		/*
 		pac_x = pacman.getX();
 		pac_y = pacman.getY();
 		ghost_x = _stategamepacman.getGhostState(0).getX();
-		ghost_y = _stategamepacman.getGhostState(0).getY();
+		ghost_y = _stategamepacman.getGhostState(0).getY(); */
 
-		//region
-
-		/*
-		//Get closest ghost
-		for (int i = 0; i < _stategamepacman.getNumberOfGhosts(); i++) {
-			StateAgentPacman ghost = _stategamepacman.getGhostState(i);
-			ghost_x = ghost.getX();
-			ghost_y = ghost.getY();
-
-			int distance = Math.abs(ghost_x - pac_x) + Math.abs(ghost_y - pac_y);
-			if ( distance < distanceGhost && distance < 4) {
-				distanceGhost = distance;
-				directionGhost = getDirection(pac_x, pac_y, ghost_x, ghost_y);
-			}
-		} */
-
-
-		//Full Ghost Hash
-
-		/*
-		ghostHash = 0;
-		distanceGhost = 0;
-		for (int i = 0; i < _stategamepacman.getNumberOfGhosts(); i++) {
-			StateAgentPacman ghost = _stategamepacman.getGhostState(i);
-			ghost_x = ghost.getX();
-			ghost_y = ghost.getY();
-
-			int distance = Math.abs(ghost_x - pac_x) + Math.abs(ghost_y - pac_y);
-			if ( distance < 3 ) {
-				distanceGhost = distance;
-				directionGhost = getDirection(pac_x, pac_y, ghost_x, ghost_y);
-			}
-			else {
-				distanceGhost = 0;
-				directionGhost = MazePacman.NORTH;
-			}
-			ghostHash = Objects.hash(ghostHash, distanceGhost, directionGhost);
-		}*/
-
-		//endregion
-
-		foodLeft = _stategamepacman.getMaze().getNbfood() - _stategamepacman.getCapsulesEaten();
+		foodLeft = StateGameFunctions.getFoodLeft(_stategamepacman);
 		foodEaten = _stategamepacman.getCapsulesEaten();
 
 		distanceDot = _stategamepacman.getClosestDot(pacman);
-		directionDot = getDirectionNextDot(_stategamepacman);
 
-		StateAgentPacman closestGhost = getClosestGhost(_stategamepacman, 3);
+		StateAgentPacman closestGhost = StateGameFunctions.getClosestGhost(_stategamepacman, 3);
 
-		directionGhost = getDirection(pacman, closestGhost);
-		distanceGhost = getDistance(pacman, closestGhost);
+		//directionGhost = StateGameFunctions.getDirectionClosestGhost(_stategamepacman);
+
+        //good hash
+        directionDot = StateGameFunctions.getDirectionNextDot(_stategamepacman);
+        directionGhost = StateGameFunctions.getDirectionClosestGhost(_stategamepacman, 4);
+        distanceGhost = StateGameFunctions.getDistance(pacman, closestGhost, 4);
+
+        //previously bad hash?
+        /*
+        distanceDot = _stategamepacman.getClosestDot(pacman);
+        directionGhost = StateGameFunctions.getDirection(pacman, closestGhost);
+		distanceGhost = StateGameFunctions.getDistance(pacman, closestGhost); */
 
 		//Coordinate ghost
-		ghostHash = getGhostPosHash(_stategamepacman);
+		//ghostHash = StateGameFunctions.getGhostPosHash(_stategamepacman);
 	}
 
 	@Override
@@ -99,7 +68,12 @@ public class EtatPacmanMDPClassic implements Etat , Cloneable{
 
         //return Objects.hash(distanceDot, ghost_x, ghost_y);
 
-        return Objects.hash(distanceDot, distanceGhost, directionGhost);
+        //was bad
+        //return Objects.hash(distanceDot, distanceGhost, directionGhost);
+
+        return Objects.hash(directionDot, distanceGhost, directionGhost);
+
+
 		//return Objects.hash(distanceDot, ghostHash, foodLeft);
 		//return Objects.hash(distanceDot, ghostHash);
 
@@ -109,98 +83,6 @@ public class EtatPacmanMDPClassic implements Etat , Cloneable{
 		//return Objects.hash(distanceDot, ghostHash, foodLeft);
 		//return Objects.hash(distanceDot, ghost_x, ghost_y);
 
-	}
-
-
-	private int getGhostPosHash(StateGamePacman stateGame) {
-		int hash = 0;
-		for (int i = 0; i < stateGame.getNumberOfGhosts(); i++) {
-			StateAgentPacman ghost = stateGame.getGhostState(i);
-			int ghost_x = ghost.getX();
-			int ghost_y = ghost.getY();
-
-			hash *= 10;
-			hash += ghost_x;
-			hash *= 10;
-			hash += ghost_y;
-			//hash = Objects.hash(hash, ghost_x, ghost_y);
-		}
-		return hash;
-	}
-
-	private int getDirectionNextDot(StateGamePacman stateGame) {
-	    StateAgentPacman pacman = stateGame.getPacmanState(0);
-	    int currentDotDistance = stateGame.getClosestDot(pacman);
-        int bestDir = 0;
-	    //4 dir + map
-	    for (int dir = 0; dir < 5; dir++) {
-
-            StateGamePacman nextGameState = stateGame.nextStatePacman(new ActionPacman(dir));
-            if ( nextGameState.getClosestDot(nextGameState.getPacmanState(0)) <= currentDotDistance ) {
-                bestDir = dir;
-                currentDotDistance = nextGameState.getClosestDot(nextGameState.getPacmanState(0));
-            }
-        }
-
-	    return bestDir;
-    }
-
-	private StateAgentPacman getClosestGhost(StateGamePacman stateGame, int maxDistance) {
-
-		StateAgentPacman pacman = stateGame.getPacmanState(0);
-		StateAgentPacman closestGhost = null;
-
-		int minDistanceGhost = Integer.MAX_VALUE;
-
-		for (int i = 0; i < stateGame.getNumberOfGhosts(); i++) {
-			StateAgentPacman ghost = stateGame.getGhostState(i);
-			int distance = getDistance(pacman, ghost);
-
-			if ( minDistanceGhost > distance && distance <= maxDistance) {
-				minDistanceGhost = distance;
-				closestGhost = ghost;
-			}
-		}
-
-		return closestGhost;
-	}
-
-	private int getDistance(StateAgentPacman pacman, StateAgentPacman ghost) {
-		return (ghost != null) ? Math.abs(ghost.getX() - pacman.getX()) + Math.abs(ghost.getY() - pacman.getY()) : 0;
-	}
-
-	private int getDirection(StateAgentPacman pacman, StateAgentPacman ghost) {
-		return (ghost != null) ? getDirection( pacman.getX(), pacman.getY(), ghost.getX(), ghost.getY()) : 0;
-	}
-
-	private int getDirection(int x1, int y1, int x2, int y2) {
-
-		float angle = getAngle(x1, y1, x2, y2);
-		if (angle < 45) {
-			return MazePacman.EAST;
-		}
-		else if (angle < 135) {
-			return MazePacman.NORTH;
-		}
-		if (angle < 225 ) {
-			return MazePacman.WEST;
-		}
-		else if (angle < 315){
-			return MazePacman.SOUTH;
-		}
-		else {
-			return MazePacman.EAST;
-		}
-	}
-
-	private float getAngle(int x1, int y1, int x2, int y2) {
-		float angle = (float) Math.toDegrees(Math.atan2(y2 - y1, x2 - x1));
-
-		if(angle < 0){
-			angle += 360;
-		}
-
-		return angle;
 	}
 
 
---
+++ src/pacman/environnementRL/StateGameFunctions.java
@@ -0,0 +1,183 @@
+package pacman.environnementRL;
+
+import pacman.elements.ActionPacman;
+import pacman.elements.MazePacman;
+import pacman.elements.StateAgentPacman;
+import pacman.elements.StateGamePacman;
+
+import javax.swing.plaf.nimbus.State;
+import java.util.ArrayList;
+
+/***
+ * Des fonctions utiles utilisé pour différencier lees états ou comme FeatureFonction du pacman à partir de StateGamePacmans
+ */
+public class StateGameFunctions {
+
+    public static int getFoodLeft(StateGamePacman stateGame) {
+        return stateGame.getMaze().getNbfood() - stateGame.getCapsulesEaten();
+    }
+
+    /**
+     * Renvoie un hash unique représentant les coordonnées des fantomes de stateGame
+     * @param stateGame
+     * @return
+     */
+    public static int getGhostPosHash(StateGamePacman stateGame) {
+        int hash = 0;
+        for (int i = 0; i < stateGame.getNumberOfGhosts(); i++) {
+            StateAgentPacman ghost = stateGame.getGhostState(i);
+            int ghost_x = ghost.getX();
+            int ghost_y = ghost.getY();
+
+            hash *= 10;
+            hash += ghost_x;
+            hash *= 10;
+            hash += ghost_y;
+        }
+        return hash;
+    }
+
+    /***
+     * Renvoie la direction (NORTH, EAST.. ) du Dot le plus proche
+     * @param stateGame
+     * @return
+     */
+    public static int getDirectionNextDot(StateGamePacman stateGame) {
+        StateAgentPacman pacman = stateGame.getPacmanState(0);
+        int currentDotDistance = stateGame.getClosestDot(pacman);
+        int bestDir = 4; //STOP
+        //4 dir + map
+        /*
+        for (int dir = 0; dir < 5; dir++) {
+
+            StateGamePacman nextGameState = stateGame.nextStatePacman(new ActionPacman(dir));
+            if ( nextGameState.getClosestDot(nextGameState.getPacmanState(0)) <= currentDotDistance ) {
+                bestDir = dir;
+                currentDotDistance = nextGameState.getClosestDot(nextGameState.getPacmanState(0));
+            }
+        } */
+
+        ArrayList<ActionPacman> validDirections = getValidDirections(stateGame);
+        for (ActionPacman action : validDirections) {
+
+            StateGamePacman nextGameState = stateGame.nextStatePacman(action);
+            if ( nextGameState.getClosestDot(nextGameState.getPacmanState(0)) <= currentDotDistance ) {
+                bestDir = action.getDirection();
+                currentDotDistance = nextGameState.getClosestDot(nextGameState.getPacmanState(0));
+            }
+        }
+        return bestDir;
+    }
+
+    public static ArrayList<ActionPacman> getValidDirections(StateGamePacman stateGame) {
+
+        StateAgentPacman pacman = stateGame.getPacmanState(0);
+        ArrayList<ActionPacman> validDirections = new ArrayList<>();
+
+        //4 direction (STOP = 4)
+        for (int dir = 0; dir < 4; dir ++) {
+            ActionPacman move = new ActionPacman(dir);
+
+            //If the next tile in this direction is not a wall
+            if (!stateGame.getMaze().isWall(pacman.getX() + move.getX(), pacman.getY() + move.getY())) {
+                validDirections.add(move);
+            }
+
+        }
+
+        return validDirections;
+    }
+    /***
+     * Renvoie le fantome le plus proche de Pacman
+     * @param stateGame
+     * @param maxDistance
+     * @return
+     */
+    public static StateAgentPacman getClosestGhost(StateGamePacman stateGame, int maxDistance) {
+
+        StateAgentPacman pacman = stateGame.getPacmanState(0);
+        StateAgentPacman closestGhost = null;
+
+        int minDistanceGhost = Integer.MAX_VALUE;
+
+        for (int i = 0; i < stateGame.getNumberOfGhosts(); i++) {
+            StateAgentPacman ghost = stateGame.getGhostState(i);
+            int distance = getDistance(pacman, ghost);
+
+            if ( distance < minDistanceGhost  && distance <= maxDistance) {
+                minDistanceGhost = distance;
+                closestGhost = ghost;
+            }
+        }
+
+        return closestGhost;
+    }
+
+    /**
+     * Renvoie la distance entre les deux agents
+     * @param pacman
+     * @param ghost
+     * @return
+     */
+    public static int getDistance(StateAgentPacman pacman, StateAgentPacman ghost) {
+        return (ghost != null) ? Math.abs(ghost.getX() - pacman.getX()) + Math.abs(ghost.getY() - pacman.getY()) : 0;
+    }
+
+    public static int getDistance(StateAgentPacman pacman, StateAgentPacman ghost, int maxDistance) {
+        int distance = (ghost != null) ? Math.abs(ghost.getX() - pacman.getX()) + Math.abs(ghost.getY() - pacman.getY()) : 0;
+        return (distance <= maxDistance) ? distance : 0;
+    }
+
+    /***
+     * Renvoie la direction (NORTH, EAST...) vers laquelle l'agent ghost se trouve par rapport à l'agent pacman.
+     * @param pacman
+     * @param ghost
+     * @return
+     */
+    public static int getDirection(StateAgentPacman pacman, StateAgentPacman ghost) {
+        return (ghost != null) ? getDirection( pacman.getX(), pacman.getY(), ghost.getX(), ghost.getY()) : 0;
+    }
+
+    public static int getDirectionClosestGhost(StateGamePacman stateGame, int maxDistance) {
+        StateAgentPacman ghost = getClosestGhost(stateGame, maxDistance);
+
+        if (ghost == null)
+            return MazePacman.STOP;
+        else
+            return getDirection(stateGame.getPacmanState(0), getClosestGhost(stateGame, maxDistance));
+    }
+
+    public static int getDirectionClosestGhost(StateGamePacman stateGame) {
+        return getDirectionClosestGhost(stateGame, 1000);
+    }
+
+    public static int getDirection(int x1, int y1, int x2, int y2) {
+
+        float angle = getAngle(x1, y1, x2, y2);
+        if (angle < 45) {
+            return MazePacman.EAST;
+        }
+        else if (angle < 135) {
+            return MazePacman.NORTH;
+        }
+        if (angle < 225 ) {
+            return MazePacman.WEST;
+        }
+        else if (angle < 315){
+            return MazePacman.SOUTH;
+        }
+        else {
+            return MazePacman.EAST;
+        }
+    }
+
+    public static float getAngle(int x1, int y1, int x2, int y2) {
+        float angle = (float) Math.toDegrees(Math.atan2(y2 - y1, x2 - x1));
+
+        if(angle < 0){
+            angle += 360;
+        }
+
+        return angle;
+    }
+}
